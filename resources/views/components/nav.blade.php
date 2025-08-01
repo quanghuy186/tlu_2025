9a22ba20baf6788d05f9af9cc379d447a5df2a70
quanghuy186 <quanghuy180623@gmail.com>
2025-08-01 08:53:14 +0700

    remove ads

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -12,7 +12,7 @@
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1802811099891545"
      crossorigin="anonymous"></script>
     <!-- Ha Noi, Viet Nam -->
-    <ins class="adsbygoogle"
+    {{-- <ins class="adsbygoogle"
         style="display:block"
         data-ad-client="ca-pub-1802811099891545"
         data-ad-slot="7709112743"
@@ -20,7 +20,7 @@
         data-full-width-responsive="true"></ins>
     <script>
         (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
+    </script> --}}
     <script>
         @if(session('success'))
             toastr.success("{{ session('success') }}");
